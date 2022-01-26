9e7a0b8dbac36f9b83f388f0ef6933a0999f894c
Abderrahim Id boubker <idboubker98@gmail.com>
2022-01-26 23:55:44 +0100

    server.service.ts

--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+// import { tap, throwError } from 'rxjs/operators';
+import { CustomResponse } from '../interface/custom-response';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ServerService {
 
-  constructor() { }
+  private readonly apiUrl = "any ";
+
+  // we can use this client to recal to the backend so we can retrieve data from the server
+  // and then map this data to our application state
+  constructor(private http: HttpClient) { }
+
+  // Methods
+  servers$ = <Observable<CustomResponse>>
+   this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+    .pipe(
+      tap(console.log),
+      catchError(this.handleError)
+    );
+  // the $ sign means that this is an observable
+  // the normal way to do this is to use the next method
+  /*
+  getServers(): Observable<CustomResponse> {
+     return this.http.get<CustomResponse>(`http://localhost:8080/server/list`); }
+   */
+
+  handleError(handleError: any): Observable<never> {
+   return  throwError  ('Method not implemented.'); 
+  }
 }
